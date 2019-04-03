04087bc98e3d3323f23c458fab6e12248fbafade
fuli.shen <fuli.shen@9icaishi.net>
2019-04-03 22:41:39 +0800

    add xgb

---
+++ train/xgb_tfidf_ens.py
@@ -0,0 +1,22 @@
+# -*- encoding:utf-8 -*-
+import sys
+
+sys.path.append("/home/julyedu_41116/2017-CCF-BDCI-AIJudge")
+
+import numpy as np
+import pandas as pd
+from gensim.models import Doc2Vec
+from utils import LOGGER
+from config.db_config import Config
+
+import warnings
+
+warnings.filterwarnings('ignore')
+config = Config()
+
+
+def main():
+    print("hello")
+
+if __name__ == "__main__":
+    main()
